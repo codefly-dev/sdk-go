b9134231ed7ecf2f481906c37a299e6ea41d027c
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-10-11 09:25:27 +0200

    fix: env parsing wip

--- codefly.go
+++ codefly.go
@@ -1,6 +1,7 @@
 package codefly
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
@@ -9,10 +10,16 @@ var networks map[string]string
 
 func init() {
 	networks = make(map[string]string)
+	Load()
+}
+
+func Load() {
+
 	for _, env := range os.Environ() {
 		if p, ok := strings.CutPrefix(env, "CODEFLY_NETWORK_"); ok {
 			tokens := strings.Split(p, "=")
 			key := strings.ToLower(tokens[0])
+			key = strings.Replace(key, "_", ".", -1)
 			value := tokens[1]
 			networks[key] = value
 		}
@@ -20,26 +27,28 @@ func init() {
 }
 
 type NetworkEndpoint struct {
-	value string
+	Value string
 }
 
 func (e *NetworkEndpoint) WithDefault(backup string) *NetworkEndpoint {
-	if e.value == "" {
-		e.value = backup
+	if e.Value == "" {
+		e.Value = backup
 	}
 	return e
 }
 
 func (e *NetworkEndpoint) Host() string {
-	return e.value
+	return e.Value
 }
 
 func (e *NetworkEndpoint) PortAddress() string {
-	return ":" + strings.Split(e.value, ":")[1]
+	fmt.Println("e.Value", e.Value)
+	return ":" + strings.Split(e.Value, ":")[1]
 }
 
 func Endpoint(name string) *NetworkEndpoint {
-	return &NetworkEndpoint{value: networks[name]}
+	fmt.Println("name", networks[name])
+	return &NetworkEndpoint{Value: networks[name]}
 }
 
 func Value(name string) bool {
--- codefly_test.go
+++ codefly_test.go
@@ -1,1 +1,32 @@
 package codefly_test
+
+import (
+	codefly "github.com/codefly-dev/sdk-go"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+//	func TestEndpointParsing(t *testing.T) {
+//		tcs := []struct {
+//			name     string
+//			endpoint string
+//			host     string
+//			port     string
+//		}{
+//			{name: "regular", endpoint: "CODEFLY_NETWORK_GUESTBOOK-GO_DEFAULT"},
+//		}
+//		for _, tc := range tcs {
+//			t.Run(tc.name, func(t *testing.T) {
+//				e := codefly.NetworkEndpoint{Value: tc.endpoint}
+//				assert.Equal(t, tc.host, e.Host())
+//			}
+//		}
+//	}
+
+func TestFromEnv(t *testing.T) {
+	err := os.Setenv("CODEFLY_NETWORK_GUESTBOOK-GO_DEFAULT", ":8080")
+	codefly.Load()
+	assert.NoError(t, err)
+	assert.Equal(t, ":8080", codefly.Endpoint("guestbook-go.default").PortAddress())
+}
